4b9ef84e4dd6ee4b942164048e930bdf717e09b5
Bisworanjan34 <bisworanjan34@gmail.com>
2024-12-27 16:28:29 +0530

    add home file jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact, faHtml5, faJava } from "@fortawesome/free-brands-svg-icons";
+import Home from './Home';
 function App() {
 
   return (
    <>
+   <Home/>
         <h2>git learning experiment </h2>
         <h1 className='font-bold text-2xl text-blue-600 uppercase'>hello dear react-developer</h1>
         <FontAwesomeIcon size='2xl' icon={faReact} style={{ margin: "0 10px", color: "#4267B2" }} />
